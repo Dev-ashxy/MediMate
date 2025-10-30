cff4560883741ae7e1e999e7b7981672d1de14fb
Asmit <asmitbar34@gmail.com>
2025-10-30 19:39:35 +0530

    Nesha kore langchain update mare

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MediMate",
+    version="0.1.0",
+    author="Asmit Barik",
+    author_email="asmitbar34@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
